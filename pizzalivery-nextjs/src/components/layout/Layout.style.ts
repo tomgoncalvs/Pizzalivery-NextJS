ae229d8a0d0d792d379fc41730b1c6c15e8088f1
Ewerton G <tonpublic97@gmail.com>
2023-10-30 23:18:39 -0300

    Add Layout Component

---
+++ pizzalivery-nextjs/src/components/layout/Layout.style.ts
@@ -0,0 +1,14 @@
+// components/layout/Layout.style.js
+
+import styled from "styled-components";
+import { colors } from "../../styles/Colors";
+
+export const ElementMain = styled.main`
+  min-height: calc(100vh - 168px);
+  background-color: ${colors.secondary.main};
+`;
+
+export const LayoutContainer = styled.div`
+  margin: 0 auto;
+  width: 80%;
+`;
---
+++ pizzalivery-nextjs/src/components/layout/Layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { Footer } from "../footer/Footer";
+import { Header } from "../header/Header";
+import { ElementMain, LayoutContainer } from "./Layout.style";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      <ElementMain>
+        <LayoutContainer>{children}</LayoutContainer>
+      </ElementMain>
+      <Footer />
+    </>
+  );
+}
